72b8a24c183fb66a3b79b13569bc4fe852094c6f
DocSJ1980 <docsaj@outlook.com>
2023-12-17 16:12:15 +0500

    Create Account page completed

--- src/app/(pages)/create-account/CreateAccountForm/index.tsx
+++ src/app/(pages)/create-account/CreateAccountForm/index.tsx
@@ -13,6 +13,7 @@ import { useAuth } from '../../../_providers/Auth'
 import classes from './index.module.scss'
 
 type FormData = {
+  name: string
   email: string
   password: string
   passwordConfirm: string
@@ -62,7 +63,8 @@ const CreateAccountForm: React.FC = () => {
         await login(data)
         clearTimeout(timer)
         if (redirect) router.push(redirect as string)
-        else router.push(`/account?success=${encodeURIComponent('Account created successfully')}`)
+        else router.push(`/`)
+        window.location.href = '/'
       } catch (_) {
         clearTimeout(timer)
         setError('There was an error with the credentials provided. Please try again.')
@@ -73,12 +75,15 @@ const CreateAccountForm: React.FC = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={classes.form}>
-      <p>
-        {`This is where new customers can signup and create a new account. To manage all users, `}
-        <Link href="/admin/collections/users">login to the admin dashboard</Link>
-        {'.'}
-      </p>
       <Message error={error} className={classes.message} />
+      <Input
+        name="name"
+        label="Fullname"
+        required
+        register={register}
+        error={errors.name}
+        type="text"
+      />
       <Input
         name="email"
         label="Email Address"
@@ -106,7 +111,7 @@ const CreateAccountForm: React.FC = () => {
       />
       <Button
         type="submit"
-        label={loading ? 'Processing' : 'Create Account'}
+        label={loading ? 'Processing' : 'Sign up'}
         disabled={loading}
         appearance="primary"
         className={classes.submit}

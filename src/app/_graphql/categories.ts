738fea2d36dbfed04c43d24c9fb36c80edf81c02
DocSJ1980 <docsaj@outlook.com>
2023-12-17 19:53:34 +0500

    Schema changes for categories added

--- src/app/_graphql/categories.ts
+++ src/app/_graphql/categories.ts
@@ -1,8 +1,23 @@
-export const CATEGORIES = `categories {
+export const PRODUCT_CATEGORIES = `categories {
   title
   id
   breadcrumbs {
     id
     label
   }
+}`
+
+export const CATEGORIES = `query categories {
+  categories(limit: 300) {
+    docs {
+      id
+      title
+      media {
+        alt
+        width
+        height
+        url
+      }
+    }
+  }  
 }`

7c2668e26e6aeb58718d68ca0ce9801191b274ed
DocSJ1980 <docsaj@outlook.com>
2023-12-24 19:09:42 +0500

    useFilter hook created

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -1,14 +1,22 @@
+"use client"
 import React from 'react'
 import classes from './index.module.scss'
 import Link from 'next/link'
 import { Category, Media } from '../../../../payload/payload-types'
+import { useFilter } from '../../../_providers/Filter'
 interface CategoryCardProps {
     category: Category
 }
 const CategoryCard = ({ category }: CategoryCardProps) => {
     const media = category.media as Media
+    const { setCategoryFilters } = useFilter()
     return (
-        <Link href='/products' className={classes.card} style={{ backgroundImage: `url(${media.url})` }}>
+        <Link
+            href='/products'
+            className={classes.card}
+            style={{ backgroundImage: `url(${media.url})` }}
+            onClick={() => setCategoryFilters([category.id])}
+        >
             <p className={classes.title}>{category.title}</p>
         </Link>
     )
---
+++ src/app/_providers/Filter/index.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { useContext, ReactNode, useState, createContext, SetStateAction } from "react"
+
+interface IContextType {
+    categoryFilters: string[],
+    setCategoryFilters: React.Dispatch<SetStateAction<string[]>>
+    sort: string,
+    setSort: React.Dispatch<SetStateAction<string>>
+}
+
+export const INITIAL_FILTER_DATA = {
+    categoryFilters: [],
+    setCategoryFilters: () => [],
+    sort: '',
+    setSort: () => ''
+}
+
+const FilterContext = createContext<IContextType>(INITIAL_FILTER_DATA)
+
+export const FilterProvider = ({ children }: { children: React.ReactNode }) => {
+    const [categoryFilters, setCategoryFilters] = useState([])
+    const [sort, setSort] = useState('-createdAt')
+    return (
+        <FilterContext.Provider value={{
+            categoryFilters,
+            setCategoryFilters,
+            sort,
+            setSort
+        }}>
+            {children}
+        </FilterContext.Provider>
+    )
+}
+
+export const useFilter = () => useContext(FilterContext)
